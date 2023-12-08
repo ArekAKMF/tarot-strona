628dbb87816cd5cc3e302f07aa8399f2ad3d9908
Arek Drewnik <arkadiusz.drewnik@akmf.pl>
2023-12-08 13:29:51 +0100

    update

--- src/components/SingTypeView.tsx
+++ src/components/SingTypeView.tsx
@@ -4,23 +4,22 @@ import { Checkerboard } from "@/components/checkerboard";
 import { Container, Heading, SimpleGrid } from "@chakra-ui/react";
 import { horoscop } from "@/utils/gameTypes";
 
-export default function SingTypeView() {
+const currentDate = () => {
+  const cd = new Date();
+  var day = cd.getDate();
+  var month = cd.getMonth() + 1;
+  var year = cd.getFullYear();
+  return day + "-" + month + "-" + year;
+};
 
+export default function SingTypeView() {
   return (
     <Container maxW="8xl">
       <Heading as="h2" variant="sectionTitle">
         Karta dnia dla znaków zodiaku
       </Heading>
-      <SimpleGrid  autoColumns="true" columns={[1,2,3]}>
+      <SimpleGrid autoColumns="true" columns={[1, 2, 3]}>
         {horoscop.map((el: any, index: number) => {
-          const currentDate = () => {
-            const cd = new Date();
-            var day = cd.getDate();
-            var month = cd.getMonth() + 1;
-            var year = cd.getFullYear();
-            return day + "-" + month + "-" + year;
-          };
-
           return (
             <Checkerboard
               key={index}
