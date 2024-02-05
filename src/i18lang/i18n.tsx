a014792fb93db9aab775d6e376f22e35fbb7efe3
Autor <arekdrewnik@gmail.com>
2024-02-05 12:23:39 +0100

    update new languages

--- src/i18lang/i18n.tsx
+++ src/i18lang/i18n.tsx
@@ -1,14 +1,17 @@
 import i18n from "i18next";
 import LanguageDetector from 'i18next-browser-languagedetector';
 import resources from '@/i18lang/resource'
+import { initReactI18next } from "react-i18next";
 
 // lng: "fr",
 
 i18n
     .use(LanguageDetector)
+    .use(initReactI18next)
     .init({
-        debug: true,
+        // debug: true,
         resources,
+        fallbackLng:"pl",
         interpolation: {
             escapeValue: false
         }
